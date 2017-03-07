66206c2645311ff0660b32a21816a5780a9a0561
Abdelrahman Awad <logaretm1@outlook.com>
2017-03-07 17:13:40 +0200

    add another field in scopes example

--- src/js/components/examples/Scopes.vue
+++ src/js/components/examples/Scopes.vue
@@ -25,6 +25,31 @@
                 </p>
             </div>
         </form>
+        <form @submit.prevent="validateForm('form-2')" class="columns column is-multiline is-12" data-vv-scope="form-2">
+            <legend>Form 2</legend>
+            <div class="column is-12">
+                <label class="label">Email</label>
+                <p class="control has-icon has-icon-right">
+                    <input name="email" v-validate="'required|email'" :class="{'input': true, 'is-danger': errors.has('form-2.email') }" type="text" placeholder="Email">
+                    <i v-show="errors.has('form-2.email')" class="fa fa-warning"></i>
+                    <span v-show="errors.has('form-2.email')" class="help is-danger">{{ errors.first('form-2.email') }}</span>
+                </p>
+            </div>
+            <div class="column is-12">
+                <label class="label">Password</label>
+                <p class="control has-icon has-icon-right">
+                    <input name="password" v-validate="'required|min:6'" :class="{'input': true, 'is-danger': errors.has('form-2.password') }" type="password" placeholder="Password">
+                    <i v-show="errors.has('form-2.password')" class="fa fa-warning"></i>
+                    <span v-show="errors.has('form-2.password')" class="help is-danger">{{ errors.first('form-2.password') }}</span>
+                </p>
+            </div>
+            <div class="column is-12">
+                <p class="control">
+                    <button class="button is-primary" type="submit" name="button">Sign up</button>
+                    <button class="button is-danger" type="button" name="button" @click="errors.clear('form-2')">Clear</button>
+                </p>
+            </div>
+        </form>
     </div>
 </template>
 
